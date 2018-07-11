b2435314bcdff7eb4bc7b8bbf5b3aa5661f3ac78
kesav <kesavaneee24@gmail.com>
2018-07-11 18:39:43 +0530

    v0.8.1

--- s3bucketcreation.tf
+++ s3bucketcreation.tf
@@ -4,14 +4,14 @@ resource "aws_s3_bucket" "s3bucketcreation" {
   policy = "${file("${path.module}/s3publicpolicy.json")}"
 
    website {
-    index_document = "./index.html"
+    index_document = "index.html"
    # error_document = "./error.html"
   }
 }
 resource "aws_s3_bucket_object" "index" {
   bucket = "${aws_s3_bucket.s3bucketcreation.bucket}"
   key = "index.html"
-  source = "./index.html"
+  source = "D:\Jees PoC\Websitecreation\terraformtest\index.html"
   content_type = "text/html"
   #etag = "${md5(file("src/index.html"))}"
 }
