d59efbaf4c7c8c1a90b97dd2ba0bb198cf37ce70
kesav <kesavaneee24@gmail.com>
2018-07-11 11:58:24 +0530

    v2.0

--- s3bucketcreation.tf
+++ s3bucketcreation.tf
@@ -1,10 +1,10 @@
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" s3bucketcreation" {
   bucket = "s3test-224617"
   acl    = "public-read"
-  policy = "${file("policy.json")}"
+  policy = "${file("D:\Jees PoC\Websitecreation\s3bucketpolicy.json")}"
 
   website {
-    index_document = "index.html"
-    error_document = "error.html"
+    index_document = "D:\Jees PoC\Websitecreation\index.html"
+    error_document = "D:\Jees PoC\Websitecreation\error.html"
   }
 }
